adce1c5ee4aa9911c66f4a4e3b1f3e260fdbd5c5
Josannnnn <161106154+aprillmezu@users.noreply.github.com>
2025-05-13 13:01:17 +0700

    home

--- firebase_authentication/lib/screens/home_screens.dart
+++ firebase_authentication/lib/screens/home_screens.dart
@@ -2,15 +2,17 @@ import 'package:firebase_authentication/screens/sign_in_screens.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_authentication/screens/sign_in_screens.dart';
-import 'package:firebase_authentication/screens/add_post_screens.dart';
+import 'add_post_screens.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
   Future<void> signOut(BuildContext context) async {
     await FirebaseAuth.instance.signOut();
     Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) => const SignInScreen()));
+      MaterialPageRoute(builder: (context) => const SignInScreen()),
+    );
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,14 +28,12 @@ class HomeScreen extends StatelessWidget {
           ),
         ],
       ),
-      body: const Center(
-        child: Text('Currently no posts'),
-      ),
+      body: const Center(child: Text('Currently no posts')),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.of(context).push(
-            MaterialPageRoute(builder: (context) => AddPostScreen()),
-          );
+          Navigator.of(
+            context,
+          ).push(MaterialPageRoute(builder: (context) => AddPostScreen()));
         },
         child: const Icon(Icons.add),
       ),
