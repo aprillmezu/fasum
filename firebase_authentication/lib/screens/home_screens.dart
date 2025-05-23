88db5d50f8f31450ffeb9c0a29ae95355e91aec8
RevinaTrisnaAini <revinatrisnaaini_2226240102@mhs.mdp.ac.id>
2025-05-23 10:27:44 +0700

    detail

--- firebase_authentication/lib/screens/home_screens.dart
+++ firebase_authentication/lib/screens/home_screens.dart
@@ -1,34 +1,256 @@
-import 'package:firebase_authentication/screens/sign_in_screens.dart';
+import 'dart:convert';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:firebase_authentication/screens/sign_in_screens.dart';
-import 'add_post_screens.dart';
+import 'package:fasum/screens/sign_in_screen.dart';
+import 'package:fasum/screens/add_post_screen.dart';
+import 'package:intl/intl.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
-  Future<void> signOut(BuildContext context) async {
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  String? selectedcategory;
+  List<String> categories = [
+    'Jalan Rusak',
+    'Marka Pudar',
+    'Lampu Mati',
+    'Trotoar Rusak',
+    'Rambu Rusak',
+    'Jembatan Rusak',
+    'Sampah Menumpuk',
+    'Saluran Tersumbat',
+    'Sungai Tercemar',
+    'Sampah Sungai',
+    'Pohon Tumbang',
+    'Taman Rusak',
+    'Fasilitas Rusak',
+    'Pipa Bocor',
+    'Vandalisme',
+    'Banjir',
+    'Lainnya',
+  ];
+
+  String formatTime(DateTime dateTime) {
+    final now = DateTime.now();
+    final diff = now.difference(dateTime);
+    if (diff.inSeconds < 60) {
+      return '${diff.inSeconds} Secs ago';
+    } else if (diff.inMinutes < 60) {
+      return '${diff.inMinutes} Mins ago';
+    } else if (diff.inHours < 24) {
+      return '${diff.inHours} Hrs ago';
+    } else if (diff.inHours < 48) {
+      return '1 Day ago';
+    } else {
+      return DateFormat('dd/MM/yyyy').format(dateTime);
+    }
+  }
+
+  Future<void> signOut() async {
     await FirebaseAuth.instance.signOut();
-    Navigator.of(context).pushReplacement(
+    if (!mounted) return;
+    Navigator.pushAndRemoveUntil(
+      context,
       MaterialPageRoute(builder: (context) => const SignInScreen()),
+          (route) => false,
     );
+  }
+
+  void _showcategoryFilter() async {
+    final result = await showModalBottomSheet<String?>(
+      context: context,
+      isScrollControlled: true,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(24)),
+      ),
+      builder: (context) {
+        return SafeArea(
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height * 0.5,
+            child: ListView(
+              padding: const EdgeInsets.only(bottom: 24),
+              children: [
+                ListTile(
+                  leading: const Icon(Icons.clear),
+                  title: const Text('Semua Kategori'),
+                  onTap: () => Navigator.pop(context, null),
+                ),
+                const Divider(),
+                ...categories.map(
+                      (category) => ListTile(
+                    title: Text(category),
+                    trailing:
+                    selectedcategory == category
+                        ? Icon(
+                      Icons.check,
+                      color: Theme.of(context).colorScheme.primary,
+                    )
+                        : null,
+                    onTap: () => Navigator.pop(context, category),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+    if (result != null) {
+      setState(() {
+        selectedcategory = result;
+      });
+    } else {
+      setState(() {
+        selectedcategory = null;
+      });
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Theme.of(context).scaffoldBackgroundColor,
       appBar: AppBar(
-        title: const Text('Home'),
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: const Text(
+          'Fasum',
+          style: TextStyle(color: Colors.green, fontWeight: FontWeight.bold),
+        ),
         actions: [
           IconButton(
-            onPressed: () {
-              signOut(context);
-            },
-            icon: const Icon(Icons.logout),
+            onPressed: _showcategoryFilter,
+            icon: Icon(Icons.filter_list),
+            tooltip: "Filter Kategori",
           ),
+          IconButton(onPressed: signOut, icon: const Icon(Icons.logout)),
         ],
       ),
-      body: const Center(child: Text('Currently no posts')),
+      body: RefreshIndicator(
+        child: StreamBuilder(
+          stream:
+          FirebaseFirestore.instance
+              .collection('posts')
+              .orderBy('createdAt', descending: true)
+              .snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return const Center(child: CircularProgressIndicator());
+            }
+            final posts =
+            snapshot.data!.docs.where((doc) {
+              final data = doc.data();
+              final category =
+                  data['Category'] ?? data['category'] ?? 'Lainnya';
+              return selectedcategory == null ||
+                  selectedcategory == category;
+            }).toList();
+
+            if (posts.isEmpty) {
+              return const Center(
+                child: Text("Tidak Ada Laporan Untuk Kategori Ini"),
+              );
+            }
+            return ListView.builder(
+              physics: const AlwaysScrollableScrollPhysics(),
+              itemCount: posts.length,
+              itemBuilder: (context, index) {
+                final data = posts[index].data();
+                final ImageBase64 = data['image'];
+                final description = data['description'];
+                final createdAt =
+                data['createdAt'] is Timestamp
+                    ? (data['createdAt'] as Timestamp).toDate()
+                    : (data['createdAt'] is String
+                    ? DateTime.parse(data['createdAt'])
+                    : DateTime.now());
+                final fullName = data['fullName'] ?? 'Anonim';
+                final latitude = data['latitude'];
+                final longitude = data['longitude'];
+                final category =
+                    data['Category'] ?? data['category'] ?? 'Lainnya';
+                String heroTag =
+                    'fasum-image-${createdAt.millisecondsSinceEpoch}';
+
+                return InkWell(
+                  onTap: () {},
+                  child: Card(
+                    elevation: 1,
+                    color: Theme.of(context).colorScheme.surfaceContainerLow,
+                    shadowColor: Theme.of(context).colorScheme.shadow,
+                    margin: const EdgeInsets.all(10),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        if (ImageBase64 != null)
+                          ClipRRect(
+                            borderRadius: const BorderRadius.vertical(
+                              top: Radius.circular(10),
+                            ),
+                            child: Hero(
+                              tag: heroTag,
+                              child: Image.memory(
+                                base64Decode(ImageBase64),
+                                fit: BoxFit.cover,
+                                width: double.infinity,
+                                height: 200,
+                              ),
+                            ),
+                          ),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 16,
+                            vertical: 8,
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                fullName,
+                                style: const TextStyle(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                formatTime(createdAt),
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  color: Colors.grey,
+                                ),
+                              ),
+                              const SizedBox(height: 12),
+                              Text(
+                                category ?? '',
+                                style: TextStyle(fontSize: 12),
+                              ),
+                              const SizedBox(height: 12),
+                              Text(
+                                description ?? '',
+                                style: const TextStyle(fontSize: 16),
+                                maxLines: 3,
+                                overflow: TextOverflow.ellipsis,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+          },
+        ),
+        onRefresh: () async {},
+      ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Navigator.of(
